59bfddce48d091bd337f3c59f80369a5bcf57852
Wiebenga, R. <r.wiebenga@umcutrecht.nl>
2024-09-04 03:20:39 +0200

    Added https://renswiebengadevelopment.onrender.com as mailserver

--- src/routes/Index/components/GetInTouch/GetInTouch.tsx
+++ src/routes/Index/components/GetInTouch/GetInTouch.tsx
@@ -20,13 +20,16 @@ export function GetInTouch() {
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    let response = await fetch("/api/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, email, subject, message }),
-    });
+    let response = await fetch(
+      "https://renswiebengadevelopment.onrender.com/api/contact",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, subject, message }),
+      }
+    );
 
     let result = await response.json();
 
